49289a5de29517ee43d2fb1baac305e73b0230e5
Vlad <53918010+VladimirUE4@users.noreply.github.com>
2020-08-28 20:24:23 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+files {
+    "astronaut.png",
+    "background.jpg",
+    "planet1.png",
+    "planet2.png",
+    "rocket.png",
+    "text.png",
+    "parallax.js",
+    "style.css",
+    "index.html",
+    "jul.mp3"
+}
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
